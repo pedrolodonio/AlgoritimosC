6fd1b043ea9c72fa249588bd5474ba24f44e60d5
pedrolodonio <99705704+pedrolodonio@users.noreply.github.com>
2022-02-22 09:49:56 -0300

    Add files via upload

---
+++ repetição(1002).c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,x,i;
+    for(i=0 ; i<3; i++){
+	printf("digite um numero\n");
+	scanf("%d",&a);
+	printf("digite outro numero\n");
+	scanf("%d",&b);
+	x=a+b;
+	printf("o valor de x e:%d \n",x);}
+}
